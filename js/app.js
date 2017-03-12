fb0516541d9604bb81d69e6b2a1ea88852d86b8e
Ervis-Priftis <steliospriftis@gmail.com>
2017-03-12 18:56:54 +0000

    js functionality TimeField added

--- js/app.js
+++ js/app.js
@@ -9,12 +9,14 @@ window.addEventListener('load', function() {
 
   // Buttons Container
   playButton = document.getElementById('play-button');
+  timeField = document.getElementById('time-field');
 
   video.load();
   video.addEventListener('canplay', function() {
+
     playButton.addEventListener('click', playOrPause, false);
     pbarContainer.addEventListener('click', skip, false);
-
+    updatePlayer();
   }, false);
 
 }, false);
@@ -34,11 +36,12 @@ function playOrPause() {
 function updatePlayer() {
   var percentage = (video.currentTime/video.duration) * 100;
   pbar.style.width = percentage + '%';
+  timeField.innerHTML = getFormattedTime();
   if (video.ended) {
     window.clearInterval(update);
     playButton.src = 'images/replay.png';
   }
-  console.log(pbar.style.width);
+  // console.log(pbar.style.width);
 }
 
 function skip(ev) {
@@ -48,6 +51,20 @@ function skip(ev) {
 
   video.currentTime = (mouseX/barWidth) * video.duration;
   updatePlayer();
-  console.log(mouseX / barWidth) * video.duration;
+  // console.log(mouseX / barWidth) * video.duration;
   // console.log(barWidth);
+}
+
+function getFormattedTime(arguments) {
+  var seccods = Math.round(video.currentTime);
+  var mitutes = Math.floor(seccods / 60);
+  if (mitutes > 0) seccods -= mitutes * 60;
+  if (seccods.toString().length === 1) seccods = '0' + seccods;
+
+  var totalSeconds = Math.round(video.duration);
+  var totalMinutes = Math.floor(totalSeconds/60);
+  if (totalMinutes > 0) totalSeconds -= totalMinutes * 60;
+  if (totalSeconds.toString().length === 1) totalSeconds = '0' + totalSeconds;
+
+  return mitutes + ':' + seccods + ' / ' + totalMinutes + ':' + totalSeconds;
 }
