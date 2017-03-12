131f20a9bf4cc573e15d775ae624d155d0840a98
Ervis-Priftis <steliospriftis@gmail.com>
2017-03-12 15:58:36 +0000

    js functionality added on proggres bar and replay button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 window.addEventListener('load', function() {
 
+  // Video Container
   video = document.getElementById('video');
 
+  // Progress Bar Container
+  pbar = document.getElementById('pbar');
+
+  // Buttons Container
   playButton = document.getElementById('play-button');
 
   video.load();
   video.addEventListener('canplay', function() {
     playButton.addEventListener('click', playOrPause, false);
-    
+
   }, false);
 
 }, false);
@@ -16,8 +21,20 @@ function playOrPause() {
   if (video.paused) {
     video.play();
     playButton.src = 'images/pause.png';
+    update = setInterval(updatePlayer, 30);
   } else {
     video.pause();
     playButton.src = 'images/play.png';
+    window.clearInterval(update);
   }
+}
+
+function updatePlayer() {
+  var percentage = (video.currentTime/video.duration) * 100;
+  pbar.style.width = percentage + '%';
+  if (video.ended) {
+    window.clearInterval(update);
+    playButton.src = 'images/replay.png';
+  }
+  console.log(pbar.style.width);
 }
